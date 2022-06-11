05c98d2a6c48777b8d16ae66800b6825e2976cdd
ekfkawl <>
2022-06-11 13:32:43 +0900

    22aaa22

--- Java/001 입출력과 사칙연산/MainA.java
+++ Java/001 입출력과 사칙연산/MainA.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main
+public class MainA
 {
     public static void main(String[] args)
     {
@@ -10,5 +10,6 @@ public class Main
         
         // 출력하기
         System.out.println(a+b);
-    }    
+        System.out.println(a-b);
+    }
 }
--- Java/001 입출력과 사칙연산/MainB.java
+++ Java/001 입출력과 사칙연산/MainB.java
@@ -1,4 +1,4 @@
-public class Main
+public class MainB
 {
     public static void main(String[] args)
     {
